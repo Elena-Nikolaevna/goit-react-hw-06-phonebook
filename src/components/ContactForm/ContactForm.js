e8c47e1a3de7a5bbf7b530fc9efe5cc8bae345b6
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-22 16:29:53 +0300

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,9 +1,70 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
+//import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
 
-class ContactForm extends Component {
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const changeInputName = event => {
+    setName(event.currentTarget.value);
+  };
+
+  const changeInputNumber = event => {
+    setNumber(event.currentTarget.value);
+  };
+
+  const hendleSubmit = event => {
+    event.preventDefault();
+    const data = { name, number };
+    onSubmit(data);
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={hendleSubmit} className={css.formWrap}>
+      <div className={css.inputWrap}>
+        <label className={css.label}>Name</label>
+        <input
+          className={css.input}
+          value={name}
+          onChange={changeInputName}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+      <div className={css.inputWrap}>
+        <label className={css.label}>Number</label>
+        <input
+          className={css.input}
+          value={number}
+          onChange={changeInputNumber}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </div>
+      <button className={css.button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+/* class ContactForm extends Component {
     state = {
         name: '',
         number: ''
@@ -66,4 +127,4 @@ ContactForm.propTypes = {
     onSubmit: PropTypes.func.isRequired,
 };
 
-export default ContactForm;
+export default ContactForm; */
