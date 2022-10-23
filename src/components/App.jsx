9ccd0fed1f63469b29e86108808961106127dece
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-23 22:30:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,65 @@
 import s from './App.module.css';
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import ContactItem from './ContactItem/ContactItem';
+import {useState,useEffect} from "react";
+import { nanoid } from 'nanoid';
 
 
-class App extends Component { 
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: ''
-  };
+function App() { 
+  const [contacts, setContacts] = useState(()=>JSON.parse(localStorage.getItem('contacts')) ?? []);
+  const [filter, setFilter] = useState('');
 
-  // Xранение контактов телефонной книги в localStorage
-  componentDidUpdate(prevState, prevProps) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+   useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts))
+   }, [contacts])
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId))
+};
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  const addContacts = data => {
+    const contact = {
+      id: nanoid(),
+      name: data.name,
+      number: data.number,
+    };
 
-
-
-  addContacts = data => { 
-    const { contacts } = this.state;
-    const names = contacts.map(contact => contact.name.toLowerCase());
-
-    //Запрет добавлять контакты, имена которых уже есть в телефонной книге
-    names.includes(data.name.toLowerCase())
-      ? alert(`${data.name} is already in contact`)
-      : this.setState(prevState => ({
-          contacts: [data, ...prevState.contacts],
-      }))
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getVisibleContact = () => {
-    const { filter, contacts } = this.state;
-    const normalizeFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizeFilter),
+    if (contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    };
+    setContacts(prevState => [contact, ...prevState]
     );
   };
-  
-  render() {
-    const { filter } = this.state;
-    const visibleContact = this.getVisibleContact();
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+
+  const visibleContact = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter),
+  );
 
     return (
       <>
           <div className={s.container}>
             <h1 className={s.title}>Phonebook</h1>
-            <ContactForm onSubmit={this.addContacts} />
+            <ContactForm onSubmit={addContacts} />
             <h2 className={s.title}>Contacts</h2>
-            <Filter value={filter} onChange={this.changeFilter} />
+            <Filter value={filter} onChange={changeFilter} />
             <ContactList>
                 <ContactItem contacts={visibleContact}
-                onDeleteContact={this.deleteContact}/>
+                onDeleteContact={deleteContact}/>
             </ContactList>
           </div>
       </>
     );
   }
-}
+
 
 export default App;
