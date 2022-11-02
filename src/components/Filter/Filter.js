234860b2aada3036fe4c415ab4a6ec37f4936520
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-03 01:40:11 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+//import React from 'react';
+///import PropTypes from 'prop-types';
 import css from './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from 'redux/contactsSlice';
 
+const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.contacts.filter);
 
-const Filter = ({ filter, onChange }) => {
-    return (
-        <div className={css.filterWrap}>
-            <label className={css.label}>Find contact by name</label>
-            <input
-                className={css.input}
-                value={filter}
-                type='text'
-                onChange={onChange}
-            ></input>
-        </div>
-    );
+  const handleFilterChange = e => {
+    dispatch(setFilter(e.target.value));
+  };
+  return (
+    <div className={css.filterWrap}>
+      <label className={css.label}>Find contact by name</label>
+      <input
+        className={css.input}
+        value={filter}
+        type="text"
+        onChange={handleFilterChange}
+      ></input>
+    </div>
+  );
 };
 export default Filter;
 
-Filter.propTypes = {
-    filter: PropTypes.string,
-    onChange: PropTypes.func.isRequired,
-  
-};
-
+/* Filter.propTypes = {
+  filter: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+}; */
