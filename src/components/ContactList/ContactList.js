912bbb62cbd56c85c3c5d8e1d3fcb69f92bd8648
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-03 01:40:08 +0200

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
+import ContactItem from '../ContactItem/ContactItem';
+import {  useSelector } from 'react-redux';
 
-const ContactList = ({ children }) => (
+const ContactList = () => {
+  const filter = useSelector(state => state.contacts.filter);
+  const contacts = useSelector(state => state.contacts.items);
+
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <>
+      <h2 className={css.title}>Phonebook</h2>
+
+      {filteredContacts.map(contact => (
+        <ContactItem
+          key={contact.id}
+          name={contact.name}
+          number={contact.number}
+          id={contact.id}
+        />
+      ))}
+    </>
+  );
+};
+
+/* (
+	
 	<> 
 		<ul className={css.contactsList}>
 			{children}
 		</ul>
 	</>
-);
+); */
 
-ContactList.propTypes = {
-    children: PropTypes.node.isRequired,
-};
+/* ContactList.propTypes = {
+  children: PropTypes.node.isRequired,
+}; */
 
 export default ContactList;
