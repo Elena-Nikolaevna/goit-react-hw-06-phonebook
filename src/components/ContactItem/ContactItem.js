1009dd2cb1c729b8699a3bd1a10563c05819daeb
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-03 01:40:06 +0200

    Update ContactItem.js

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -1,9 +1,29 @@
-
-import React from 'react';
+//import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactItem.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteItems } from 'redux/contactsSlice';
 
-const ContactItem = ({ contacts, onDeleteContact }) => (
+const ContactItem = ({ name, number, id }) => {
+  const dispatch = useDispatch();
+  const onDeleteContact = contactId => dispatch(deleteItems(contactId));
+  return (
+    <li key={id} className={css.contactsItem}>
+      <p className={css.contactText}>
+        {name}: {number}
+      </p>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => onDeleteContact(id)}
+      >
+        Delete
+      </button>
+    </li>
+  );
+};
+
+/* (
 	<>
 			{contacts.map(({ id, name, number }) => (
 				<li key={id} className={css.contactsItem}>
@@ -20,16 +40,16 @@ const ContactItem = ({ contacts, onDeleteContact }) => (
 			))}
 	</>
 );
-
+ */
 ContactItem.propTypes = {
-    contacts: PropTypes.arrayOf(
-		PropTypes.exact({
-		  name: PropTypes.string.isRequired,
-		  number: PropTypes.string.isRequired,
-		  id: PropTypes.string.isRequired,
-		})
-	  ),
-    onDeleteContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
 };
 
 export default ContactItem;
